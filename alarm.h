11557f60e7fbf855fba7f8dd6a01d07f03c86ae0
choeh81 <kwang1122@ewha.ac.kr>
2026-07-06 02:57:54 +0000

    lab5

--- alarm.h
+++ alarm.h
@@ -1,11 +1,3 @@
-// 3. alarm.h: alarm 클래스 정의
-// 1의 본인이름학번의 네임스페이스 안에 클래스를 정의하고 멤버함수들도 모두 인라인으로 구현합니다. 
-// private 멤버변수 선언: timeOfDay형 wakeTime, bool형 inActive
-// public 멤버함수 인라인으로 정의
-// -생성자: 모든 멤버변수 초기화, 기본값 설정 (이건 내가 쓰는거: test할필요는 없는듯 이미 test해서 보내줌)
-// -print: 표준스트림출력으로 멤버변수들 출력, hour:minute alarm is on/off 형식
-// -wakeTime의 접근함수를 참조형식으로 구현
-
 #pragma once
 #include "timeOfDay.h"
 
--- timeOfDay.h
+++ timeOfDay.h
@@ -1,9 +1,3 @@
-// 1. 본인이름학번의 네임스페이스
-// -본인이름학번 네임스페이스 예: 이름이 김프로이고 학번이 1234567일 경우 KimPro1234567
-// using 지시자는 cpp파일에서는 영역 { block } 안에서 사용, 헤더파일엔 using 지시자는 사용하지 않고 네임스페이스 지정자를 사용합니다.
-// -using 지시자 예: { using namespace std; cout << "Enter your id: "; }
-// -네임스페이스 지정자 예: std::cout << "Enter your id: ";
-
 #pragma once
 #include <iostream>
 
@@ -11,70 +5,98 @@ namespace ChoEunhye2372048
 {
     class timeOfDay
     {
-        private:
-            int hour{};
-            int minute{};
-            void testHour(){
-                if( (hour<0) || (hour>23) )
-                {
-                    std::cout << "Invaild hour\n";
-                    std::exit(1);
-                } 
-            };
-            void testMinute(){
-                if( (minute<0) || (minute>59) )
-                {
-                    std::cout << "Invaild minute\n";
-                    std::exit(1);
-                };
-            };
+    private:
+        int hour{};
+        int minute{};
+        void testHour(){
+            if( (hour<0) || (hour>23) ) {
+                std::cout << "Invaild hour\n";
+                std::exit(1);} 
+        };
+        void testMinute(){
+            if( (minute<0) || (minute>59) ) {
+                std::cout << "Invaild minute\n";
+                std::exit(1);};
+        };
 
-        public:
-            timeOfDay(int h = 0, int m = 0) : hour{h}, minute{m}
-            {testHour();
-            testMinute();}
+    public:
+        timeOfDay(int h = 0, int m = 0) : hour{h}, minute{m}
+        {testHour();
+        testMinute();}
 
-            void input(){
-                std::cout << "Enter hour(0~23): ";
-                std::cin >> hour;
-                testHour();
-                std::cout << "Enter minute(0~59): ";
-                std::cin >> minute;
-                testMinute();
-            };
+        void input(){
+            std::cout << "Enter hour(0~23): ";
+            std::cin >> hour;
+            testHour();
+            std::cout << "Enter minute(0~59): ";
+            std::cin >> minute;
+            testMinute();
+        };
         
-            void setHour(int h) {
-                hour = h; 
-                testHour();
-            }
-            void setMinute(int m){
-                minute = m;
-                testMinute();
-            }
-            void print() const{
-                if(hour<10) std::cout << "0";
-                std::cout << hour << ":" ;
-                if(minute<10) std::cout << "0";
-                std::cout << minute;
-            }
-            int getHour() const {return hour;}
-            int getMinute() const {return minute;}
+        void setHour(int h) {
+            hour = h; 
+            testHour();}
+        void setMinute(int m){
+            minute = m;
+            testMinute();}
+        void print() const{
+            if(hour<10) std::cout << "0";
+            std::cout << hour << ":" ;
+            if(minute<10) std::cout << "0";
+            std::cout << minute;}
+        int getHour() const {return hour;}
+        int getMinute() const {return minute;}
+
+
+
+
+        timeOfDay operator++(){
+            ++minute; // 0~59 사이의 값이어야 함, 59인 경우는 시간이 +1 되어야함
+            if ( minute == 60 ) { minute = 0; ++hour; }// 0~23 사이의 값이어야 함, 넘어가면 0으로 리셋
+            if( hour == 24 ) { hour = 0; }
+            return timeOfDay{hour, minute};
+        }
+        timeOfDay operator++(int){
+            timeOfDay temp{hour, minute};
+            minute++;
+            if ( minute == 60 ) { minute = 0; ++hour; }// 0~23 사이의 값이어야 함, 넘어가면 0으로 리셋
+            if( hour == 24 ) { hour = 0; }
+            return temp;
+        }
+        
+        friend std::istream& operator>>(std::istream& is, timeOfDay& t){
+        //std::cin (input()) -> is
+            std::cout << "Enter hour(0~23): "; //지워도 된다는데 이유를 모르겠음 7/6 11:17 녹화 참고
+            is >> t.hour;
+            t.testHour();
+            std::cout << "Enter minute(0~59): "; //지워도 된다는데 이유를 모르겠음 7/6 11:17 녹화 참고
+            is >> t.minute;
+            t.testMinute();
+            return is;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const timeOfDay& t){
+        //std::cout (print()) -> os
+            if(t.hour < 10) os << "0";
+            os << t.hour << ":" ;
+            if(t.minute < 10) os << "0";
+            os << t.minute;
+            return os;
+        }
+        friend bool operator==(const timeOfDay& t1, const timeOfDay& t2){
+            return t1.hour == t2.hour && t1.minute == t2.minute;
+        }
+        friend timeOfDay operator+(const timeOfDay& t1, const timeOfDay& t2){
+        //59+1=60 되면서 hour+1
+            int minute1{t1.minute + t1.hour*60};
+            int minute2{t2.minute + t2.hour*60};
+            int totalMinute{minute1+minute2};
+            int newHour{totalMinute / 60};
+            int newMinute{totalMinute % 60}; //0~23이어야 함
+            if(newHour>24) newHour = newHour %= 24; // = newHour % 24
+            return timeOfDay{newHour, newMinute};
+        }
+
     };
 };
 
-
-// 2. timeOfDay.h: timeOfDay 클래스 정의
-// 1의 본인이름학번의 네임스페이스 안에 클래스를 정의하고 멤버함수들도 모두 인라인으로 구현합니다. 
-// private 멤버변수 선언: int형 hour, int형 minute
-// private 멤버함수 정의
-// -testHour: hour는 0~23가 아니면 프로그램 종료
-// -testMinute: minute은 0~59가 아니면 프로그램 종료
-
-
-// public 멤버함수 정의
-// -생성자: 모든 멤버변수 초기화, 기본값 설정, test함수들 호출
-// -input: 표준스트림입력으로 멤버변수들 입력, test함수들 호출
-// -set 접근함수들: 멤버변수 값 설정 및 test함수 호출
-// const 멤버함수 정의
-// -print: 표준스트림출력으로 멤버변수들 출력, 1자리수면 앞에 0을 붙여줌, hour:minute형식
-// -get 접근함수들: 멤버변수 값 리턴
