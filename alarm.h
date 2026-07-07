d2ef1f84b70b3e3858c9f32175ef78edf648ff2b
choeh81 <kwang1122@ewha.ac.kr>
2026-07-07 02:43:02 +0000

    lab6

--- alarm.h
+++ alarm.h
@@ -5,18 +5,50 @@ namespace ChoEunhye2372048
 {
    class alarm
    {
-        private:
-            timeOfDay wakeTime{};
-            bool inActive{};
+    private:
+        std::string name;
+        timeOfDay wakeTime{};
+        bool inActive{};
 
-        public:
-        alarm(timeOfDay t = timeOfDay{1,1}, bool a = false) : wakeTime{t}, inActive{a} {}
-        void print()
-        {
-            wakeTime.print();
-            std::cout << " alarm is " << ( (inActive) ? "on" : "off" ) << "\n"; //inActive가 ture 면 on, false면 off
-        }
-        const timeOfDay& gethWakeTime() const {return wakeTime;}
-        void setWakeTime(const timeOfDay& t) {wakeTime = t;} //참조형식 아니고 값형식
-   };
+    public:
+    alarm(const std::string& n = "no name yet", 
+        timeOfDay t = timeOfDay{1,1}, 
+        bool a = false) 
+        : name{n}, wakeTime{t}, inActive{a} {}
+    void print() const
+    {
+        std::cout << name << " " ;
+        wakeTime.print();
+        std::cout << " alarm is " << ( (inActive) ? "on" : "off" ) << "\n"; //inActive가 ture 면 on, false면 off
+    }
+    friend std::ostream& operator<<(std::ostream& os, const alarm& a){
+        os << a.name << " " ;
+        os << a.wakeTime;
+        os << " alarm is " << ((a.inActive) ? "on" : "off") << '\n';
+        return os;
+    }
+    void input(){
+        std::cout << "Enter alarm name: ";
+        std::getline(std::cin >> std::ws, name);
+        wakeTime.input();
+        std::cout << "Enter 1 or 0 (on/off): ";
+        std::cin >> inActive;
+    }
+    friend std::istream& operator>>(std::istream& is, alarm& a){
+        //std::cin --> is
+        std::cout << "Enter alarm name: ";
+        std::getline(is >> std::ws, a.name);
+        is >> a.wakeTime;
+        std::cout << "Enter 1 or 0 (on/off): ";
+        is >> a.inActive;
+        return is;
+    }
+    const std::string& getName() const {return name;}
+    const timeOfDay& gethWakeTime() const {return wakeTime;}
+    bool getActive() const {return inActive;}
+    void setName(const std::string& n){name = n;}
+    void setWakeTime(const timeOfDay& t) {wakeTime = t;} //참조형식 아니고 값형식
+    void setActive(bool b){inActive = b;}
+
+    };
 }
--- main.cpp
+++ main.cpp
@@ -1,20 +1,7 @@
 #include "alarm.h"
 #include "timeOfDay.h"
+#include <array>
 
-int main(){
-    using namespace ChoEunhye2372048;
-    timeOfDay t1, t2;
-    std::cin >> t1 >> t2;
-    std::cout << t1 << " " << t2 << std::endl;
-    std::cout << ++t2 << std::endl;
-    std::cout << t2++ << std::endl;
-    std::cout << t2 << std::endl;
-
-    if(t1==t2) std::cout << "same\n";
-    else std::cout << "different\n";
-
-    std::cout << t1 + t2 << std::endl;
-}
 
 // namespace ChoEunhye2372048
 // {
@@ -34,4 +21,52 @@ int main(){
 //     else std::cout << "different\n";
 // };
 
+
+
+// int main(){
+//     using namespace ChoEunhye2372048;
+//     timeOfDay t1, t2;
+//     std::cin >> t1 >> t2;
+//     std::cout << t1 << " " << t2 << std::endl;
+//     std::cout << ++t2 << std::endl;
+//     std::cout << t2++ << std::endl;
+//     std::cout << t2 << std::endl;
+
+//     if(t1==t2) std::cout << "same\n";
+//     else std::cout << "different\n";
+
+//     std::cout << t1 + t2 << std::endl;
+// }
+
+namespace ChoEunhye2372048{
+    void printAlarmArray(const alarm a[], const int n){
+        for (int i=0; i<n; i++)
+            std::cout << a[i];        
+    }
+}
+
+int main(){
+    using namespace ChoEunhye2372048;
+
+    const int n{4};
+    alarm a[n];
+
+    a[0] = alarm{"morning", {6,0}, 1};
+    a[1].setName("class");
+    a[1].setWakeTime({9,0});
+    a[1].setActive(1);
+    a[2].input();
+    
+    std::cin >> a[3];
+    printAlarmArray(a,n);
+
+    std::array<alarm, n> b;
+    for(int i = 0 ; i < b.size(); ++i)
+        b.at(i) = a[i]; // b[i]
+    for (const auto& bi : b)
+        bi.print();
+
+    //std::cout << a[3];
+
+}
 
